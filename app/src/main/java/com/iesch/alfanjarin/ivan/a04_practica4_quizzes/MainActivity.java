206b2da10bb7570950e32700849933ddeaa65e36
ialfanjaring <ialfanjaring@iesch.org>
2022-10-20 01:37:19 +0200

    Initial commit

---
+++ app/src/main/java/com/iesch/alfanjarin/ivan/a04_practica4_quizzes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.iesch.alfanjarin.ivan.a04_practica4_quizzes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
