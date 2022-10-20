77b8a4b599d824a1a53b1a7a54a8f11aa16b3b2d
ialfanjaring <ialfanjaring@iesch.org>
2022-10-20 03:10:23 +0200

    Vistas creadas y boton respues creado

--- app/src/main/java/com/iesch/alfanjarin/ivan/a04_practica4_quizzes/MainActivity.java
+++ app/src/main/java/com/iesch/alfanjarin/ivan/a04_practica4_quizzes/MainActivity.java
@@ -2,13 +2,33 @@ package com.iesch.alfanjarin.ivan.a04_practica4_quizzes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.iesch.alfanjarin.ivan.a04_practica4_quizzes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        int contador = 1;
+        //setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+        binding.responderBtn.setOnClickListener(v -> {
+            String pregunta = binding.preguntaTxt.getText().toString();
+            int opcion = binding.radioGroup.getCheckedRadioButtonId();
+
+            abrirResultado(pregunta, opcion, contador);
+        });
+    }
+
+    private void abrirResultado(String pregunta, int opcion, int contador) {
+        Intent irADetalle = new Intent(this,ResultActivity.class);
+        irADetalle.putExtra("pregunta",pregunta);
+        irADetalle.putExtra("opcion",opcion);
+        irADetalle.putExtra("contador",contador);
+        startActivity(irADetalle);
     }
 }
