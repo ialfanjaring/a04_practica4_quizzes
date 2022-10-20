91084f932545a27569171f3cc9826a8f248e74df
ialfanjaring <ialfanjaring@iesch.org>
2022-10-20 10:39:24 +0200

    Funcionamiento entre Vistas

--- app/src/main/java/com/iesch/alfanjarin/ivan/a04_practica4_quizzes/MainActivity.java
+++ app/src/main/java/com/iesch/alfanjarin/ivan/a04_practica4_quizzes/MainActivity.java
@@ -7,27 +7,55 @@ import android.os.Bundle;
 
 import com.iesch.alfanjarin.ivan.a04_practica4_quizzes.databinding.ActivityMainBinding;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        Bundle extras = getIntent().getExtras();
         int contador = 1;
+        if(extras != null){
+            contador = extras.getInt("contador");
+        }
+        AtomicBoolean acierto = new AtomicBoolean(false);
+        int result = 0;
+
+
+        binding.numeroTxt.setText(contador + "/3");
+        if(contador == 1){
+            binding.preguntaTxt.setText(R.string.pregunta1);
+            binding.radioA.setText(R.string.respuestaA1);
+            binding.radioB.setText(R.string.respuestaB1);
+            binding.radioC.setText(R.string.respuestaC1);
+            binding.radioD.setText(R.string.respuestaD1);
+            result = binding.radioC.getId();
+        }
+
         //setContentView(R.layout.activity_main);
         setContentView(binding.getRoot());
+        int finalResult = result;
+
+        int finalContador = contador;
         binding.responderBtn.setOnClickListener(v -> {
             String pregunta = binding.preguntaTxt.getText().toString();
             int opcion = binding.radioGroup.getCheckedRadioButtonId();
-
-            abrirResultado(pregunta, opcion, contador);
+            if(finalResult == opcion){
+                acierto.set(true);
+            }else{
+                acierto.set(false);
+            }
+            abrirResultado(pregunta, acierto.get(), finalContador);
         });
     }
 
-    private void abrirResultado(String pregunta, int opcion, int contador) {
+    private void abrirResultado(String pregunta, boolean acierto, int contador) {
         Intent irADetalle = new Intent(this,ResultActivity.class);
         irADetalle.putExtra("pregunta",pregunta);
-        irADetalle.putExtra("opcion",opcion);
+        irADetalle.putExtra("acierto",acierto);
         irADetalle.putExtra("contador",contador);
         startActivity(irADetalle);
     }
--- app/src/main/java/com/iesch/alfanjarin/ivan/a04_practica4_quizzes/ResultActivity.java
+++ app/src/main/java/com/iesch/alfanjarin/ivan/a04_practica4_quizzes/ResultActivity.java
@@ -1,14 +1,45 @@
 package com.iesch.alfanjarin.ivan.a04_practica4_quizzes;
 
+import static com.iesch.alfanjarin.ivan.a04_practica4_quizzes.R.string.correcto;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.iesch.alfanjarin.ivan.a04_practica4_quizzes.databinding.ActivityMainBinding;
+import com.iesch.alfanjarin.ivan.a04_practica4_quizzes.databinding.ActivityResultBinding;
 
 public class ResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
+        ActivityResultBinding binding = ActivityResultBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Bundle extras = getIntent().getExtras();
+
+        String pregunta = extras.getString("pregunta");
+        binding.preguntaTxt.setText(pregunta);
+        int contador = extras.getInt("contador");
+        boolean acierto = extras.getBoolean("acierto");
+        if (acierto){
+            binding.aciertoTxt.setText(correcto);
+        }else {
+            binding.aciertoTxt.setText(R.string.incorrecto);
+        }
+        binding.continuarBtn.setOnClickListener(v -> {
+            abrirMain(contador);
+                });
+    }
+
+    private void abrirMain(int contador) {
+        Intent irAMain = new Intent(this,MainActivity.class);
+        contador++;
+        irAMain.putExtra("contador",contador);
+        startActivity(irAMain);
+
     }
 }
